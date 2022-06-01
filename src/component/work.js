ca246bc4afac7f165b1968ad228f4bdc56ae8bff
danyr59 <danyelrange59@gmail.com>
2022-06-01 10:57:39 -0400

    correccion

--- src/component/work.js
+++ src/component/work.js
@@ -2,9 +2,9 @@ import React from "react"
 import Tags from "./tags"
 import { Container, Text } from "@chakra-ui/react"
 
-const Work = ({ labels, children, text , props}) => {
+const Work = ({ labels, children, text, props }) => {
   return (
-    <Container {...props} >
+    <Container overflowX="hidden" >
       <Text m="10px">{text}</Text>
       <Tags labels={labels} />
       {children}
