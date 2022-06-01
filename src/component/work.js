f56b591097472752a5c75fef415bff77742d21ff
danyr59 <danyelrange59@gmail.com>
2022-06-01 11:13:08 -0400

    overflow

--- src/component/work.js
+++ src/component/work.js
@@ -2,9 +2,9 @@ import React from "react"
 import Tags from "./tags"
 import { Container, Text } from "@chakra-ui/react"
 
-const Work = ({ labels, children, text, props }) => {
+const Work = ({ labels, children, text }) => {
   return (
-    <Container overflowX="hidden" >
+    <Container overflow="auto" >
       <Text m="10px">{text}</Text>
       <Tags labels={labels} />
       {children}
