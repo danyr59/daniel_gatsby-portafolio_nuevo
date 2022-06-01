1378b6e75e01c0ce87308d3c12d1487461dccbbc
danyr59 <danyelrange59@gmail.com>
2022-06-01 18:16:25 -0400

    arreglo svg logo

--- src/component/logo.js
+++ src/component/logo.js
@@ -1,38 +1,59 @@
 import * as React from "react"
 import { Link } from 'gatsby'
 import { StaticImage } from "gatsby-plugin-image"
-import { Text } from "@chakra-ui/react"
-import styled from "@emotion/styled"
+import { Text, Box } from "@chakra-ui/react"
+// import styled from "@emotion/styled"
 
-const LogoBox = styled.span`
-  font-weight: bold;
-  font-size: 18px;
-  display: inline-flex;
-  align-items: center;
-  height: 50px;
-  line-height: 20px;
-  margin: 10px;
-  img {
-    padding-right: 10px;
-    transition: 200ms ease;
-  }
+/* const LogoBox = styled.span` */
+/*   font-weight: bold; */
+/*   font-size: 18px; */
+/*   display: inline-flex; */
+/*   align-items: center; */
+/*   height: 50px; */
+/*   line-height: 20px; */
+/*   margin: 10px; */
+/*   weight: "auto"; */
+/*   img { */
+/*     padding-right: 10px; */
+/*     transition: 200ms ease; */
+/*     width: "100%"; */
+/*   } */
 
-  &:hover img {
-    transform: rotate(20deg);
-  }
-`
+/*   &:hover img { */
+/*     transform: rotate(20deg); */
+/*   } */
+/* ` */
+
+// const ImageGatsby = (props) => <StaticImage {...props} />
 
 const Logo = () => {
   return (
     <Link to="/">
-      <LogoBox>
-        <StaticImage
-          width={35}
-          height={50}
-          src="../images/logo.svg"
-          alt="logo"
-          placeholder="logo"
-        />
+      {/* <LogoBox> */}
+      <Box
+        fontWeight="bold"
+        fontSize="18px"
+        display="inline-flex"
+        alignItems="center"
+        height="60px"
+        lineHeight="20px"
+        margin="10px"
+      >
+        <Box
+          w="auto"
+          paddingRight="10px"
+          transition="200ms ease"
+          _hover={{ transform: "rotate(20deg)" }}
+
+        >
+          <StaticImage
+            width={30}
+            height={45}
+            src="../images/logo.svg"
+            alt="logo"
+            placeholder="logo"
+          />
+        </Box>
 
         <Text
           // color={useColorModeValue('gray.800', 'whiteAlpha.900')}
@@ -42,7 +63,8 @@ const Logo = () => {
         >
           Daniel Rangel
         </Text>
-      </LogoBox>
+      </Box>
+      {/* </LogoBox> */}
     </Link>
   )
 }
